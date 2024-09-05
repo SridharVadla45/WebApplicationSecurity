cf42e05a66c22e889f4a0bb8ac9bf707d33f9ea9
SridharVadla <vadlasreedhar123@gmail.com>
2024-09-05 11:36:35 +0530

    initialized username-password authentication

---
+++ login-register-system/src/main/java/com/learn/security/login_register_system/LoginRegisterSystemApplication.java
@@ -0,0 +1,13 @@
+package com.learn.security.login_register_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegisterSystemApplication.class, args);
+	}
+
+}
